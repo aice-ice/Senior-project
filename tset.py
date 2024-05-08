93219dc687bd16dc8ff099b17fb730c0094174be
momonja <eu21052-6739@sti.chubu.ac.jp>
2024-05-08 19:43:26 +0900

    upload file and prototype of a residual section

---
+++ tset.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+rng = np.random.default_rng(123)
+data = np.array([[4, 15], [6, 8], [5, 17], [8, 15], [3, 1]])
+
+x = np.arange(0, 10, 0.1)
+y = 2 * x  + 1
+
+plt.plot(x, y)
+plt.scatter(data[:, 0], data[:, 1])
+plt.show()
